17d8160d3db02dbf22539d66b8088f109026027b
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2024-12-17 11:15:04 -0500

    Optimiza tu App Android: Construye un Selector de Días y Comidas con MVVM
    - Optimiza tu App Android: Construye un Selector de Días y Comidas con MVVM

--- app/src/main/java/com/platzi/android/mvvm/presentation/tracker_overview/TrackerOverviewScreen.kt
+++ app/src/main/java/com/platzi/android/mvvm/presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,11 +1,13 @@
 package com.platzi.android.mvvm.presentation.tracker_overview
 
-import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -13,7 +15,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.platzi.android.mvvm.app.ui.theme.LocalSpacing
 import com.platzi.android.mvvm.app.ui.theme.PlatziCaloriesTheme
+import com.platzi.android.mvvm.presentation.tracker_overview.components.DaySelector
+import com.platzi.android.mvvm.presentation.tracker_overview.components.ExpandableMeal
 import com.platzi.android.mvvm.presentation.tracker_overview.components.NutrientsHeader
+import com.platzi.android.mvvm.presentation.tracker_overview.model.defaultMeals
+import java.time.LocalDate
 
 @Composable
 fun TrackerOverviewScreen(
@@ -30,6 +36,36 @@ fun TrackerOverviewScreen(
         item {
             NutrientsHeader()
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = LocalDate.now(),
+                onPreviousDayClick = {
+
+                },
+                onNextDayClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+        }
+        items(defaultMeals){ meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
@@ -46,21 +82,44 @@ fun TrackOverviewScreenTest(){
     ) {
         item {
             NutrientsHeader()
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = LocalDate.now(),
+                onPreviousDayClick = {
+
+                },
+                onNextDayClick = {
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+        }
+        items(defaultMeals){ meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
 private fun TrackOverviewScreenPreview() {
-    PlatziCaloriesTheme {
-        TrackOverviewScreenTest()
-    }
-}
-
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun TrackOverviewScreenPreviewDark() {
     PlatziCaloriesTheme {
         TrackOverviewScreenTest()
     }
