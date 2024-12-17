0ccdf762198fbe0f98ceab41e98c7bf0112d87e5
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2024-12-16 21:10:52 -0500

    Configuracion de nuestro proyecto Seguidor de Calorias y Welcome Screen
    Configuracion de nuestro proyecto Seguidor de Calorias y Welcome Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,16 +2,20 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.gradle.plugin)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room)
 }
 
 android {
     namespace = "com.platzi.android.mvvm.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.platzi.android.mvvm.app"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +41,9 @@ android {
     buildFeatures {
         compose = true
     }
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
@@ -49,6 +56,49 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    //Google fonts
+    implementation(libs.androidx.ui.text.google.fonts)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.dagger.compiler)
+
+    //Navigation Compose
+    implementation(libs.jetbrains.compose.navigation)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //Coil3
+    implementation(libs.coil.compose)
+    implementation(libs.coil.compose.core)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
+    //Moshi
+    implementation(libs.moshi)
+    ksp(libs.moshi.codegen)
+    implementation(libs.moshi.kotlin)
+
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
+
+    //OkHttp
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+
+    //Room
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    //Kotlinx Serialization
+    implementation(libs.kotlinx.serialization.json)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
